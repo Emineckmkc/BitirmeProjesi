c14bfbee56c0e13db3113bd4ab21ee0c4bf8cc29
Emine Çakmakcı <55896383+Emineckmkc@users.noreply.github.com>
2021-06-11 16:22:40 +0300

    Update LSTM_word2vecModel_pretrained.py

--- src/LSTM_word2vecModel_pretrained.py
+++ src/LSTM_word2vecModel_pretrained.py
@@ -6,13 +6,15 @@ from keras.models import Sequential
 from keras.layers.core import Dense
 from keras.layers.embeddings import Embedding
 from keras.layers.core import Dropout
+from keras.layers import Flatten
 
 #csv 
 import csv
 
 #numpy
 import numpy as np
-
+#matplotlib
+import matplotlib.pyplot as plt
 #stop-words
 from nltk.corpus import stopwords
 stopWords = set(stopwords.words('turkish'))
@@ -36,32 +38,34 @@ temp=[]
 with open('TOPLAM.csv',newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
+         #liste türünde gelen cümleyi stringe çevir.
+         strcumle= row[0]
+         #String halinde olan cümleyi kelime keliime parçala.
+         kelimeler= strcumle.split()
+         #son kelimeye(*n, *p ,*nt yani etiketler) kadar olanları al.
+         metin= kelimeler[0:-1]
+         #satırın son kelimesini etiketlere ekle
+         etiketler.append(kelimeler[-1])
 
-         str2= row[0]
-         str3= str2.split()
-         str4= str3[0:-1]
-         #add the last word of the line to the tags
-         etiketler.append(str3[-1])
-
-         #check the stop words, remove if any
+        #durak kelimeleri kontrol et, varsa kaldır
          temp = []
-         for w in str4:
+         for w in metin:
             if w not in stopWords:
              temp.append(w)
          cumleler.append(temp)
 
 
-#Convert labels to numbers
+#Etiketleri sayılara dönüştür.
 le = LabelEncoder()
 etiketler = le.fit_transform(etiketler)
 
 
-
+# indirilen hazır word2vec ile eğitilmiş modeli yükle.
 from gensim.models import KeyedVectors
 w2vModel = KeyedVectors.load_word2vec_format('trmodel', binary=True)
 
-#metinlerde en çok geçen 10000 kelimenin işleme alınmasını sağlayalım
-num_max =  10000
+#metinlerde en çok geçen 30 kelimenin işleme alınmasını sağlayalım
+num_max = 30
 # tokinizer nesnesini oluştur
 tk=Tokenizer(num_words=num_max)
 
@@ -72,7 +76,7 @@ X = tk.texts_to_sequences(cumleler)
 #Çoğu algoritma giriş verilerinin sabit uzunlukta olmasını bekler. 
 #Bu durumlarda tokenizer’ın padding metodunu kullanırız.
 # her bir giriş verisininin uzunluğu yalnızca 15 olsun
-maxlentweet = 15
+maxlentweet = 22
 X = pad_sequences(X, maxlen=maxlentweet)
 
 
@@ -81,21 +85,18 @@ embedding_layer = Embedding(input_dim=w2vModel.wv.syn0.shape[0],
                             weights=[w2vModel.wv.syn0],
                             input_length=maxlentweet)
 
-
 print('Embedding_layer input_dim: ',str(w2vModel.wv.syn0.shape[0]))
 print('Embedding_layer output_dim: ',str(w2vModel.wv.syn0.shape[1]))
 print('Embedding_layer input_length: ',str(X.shape[1]))
 
-lstm_out = 20
+lstm_out = 22
 num_classes = 3
 
 model = Sequential()
 model.add(embedding_layer)
-
-model.add(LSTM(units=lstm_out,recurrent_dropout=0.1))
+model.add(LSTM(units=lstm_out,recurrent_dropout=0.5))
+model.add(Flatten())
 model.add(Dense(num_classes, activation='softmax'))
-
-
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
@@ -106,16 +107,16 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, etiketler, test_size=0.2,
 #fit model
 batch_size = 64
 EPOCHS = 10
-model.fit(X_train, Y_train, verbose=1,epochs= EPOCHS, batch_size=batch_size, validation_split = 0.1 )
+history=model.fit(X_train, Y_train, verbose=1,epochs= EPOCHS, batch_size=batch_size, validation_split = 0.1 )
 
 
-score, acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size=batch_size)
+score, acc = model.evaluate(X_test, Y_test, verbose = 1, batch_size=batch_size)
 y_pred = model.predict(X_test)
 
 scores = model.evaluate(X_test, Y_test, verbose=0)
 print("Test Accuracy: %.2f%%" % (scores[1] * 100))
 
-y_pred = model.predict(X_test, batch_size=5, verbose=1)
+y_pred = model.predict(X_test, batch_size=batch_size, verbose=1)
 y_pred2 = np.argmax(y_pred, axis=1)
 
 
@@ -123,5 +124,48 @@ print(classification_report(Y_test, y_pred2))
 
 print('------------confusion_matrix-------------------')
 print(confusion_matrix(Y_test, y_pred2, labels=[0, 1, 2]))
+
+#--------------------------------Verisetini Örnekleme----------------------------
+
+from imblearn.over_sampling import RandomOverSampler
+rus = RandomOverSampler(random_state = 0,sampling_strategy='minority')
+
+x_rus, y_rus = rus.fit_resample(X, etiketler)
+x_train, x_test, y_train, y_test = train_test_split(x_rus, y_rus, test_size = 0.25, random_state = 42)
+
+
+model2 = Sequential()
+model2.add(embedding_layer)
+model2.add(LSTM(units=lstm_out,dropout = 0.2,recurrent_dropout=0.5))
+model2.add(Dense(num_classes, activation='softmax'))
+model2.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model2.summary())
+history2 = model2.fit(x_train, y_train, validation_split=0.20, epochs=10,batch_size=64)
+
+score, acc = model2.evaluate(x_test, y_test, verbose = 1, batch_size=64)
+y_pred = model2.predict(x_test)
+
+scores = model2.evaluate(x_test, y_test, verbose=0)
+print("Test Accuracy: %.2f%%" % (scores[1] * 100))
+
+y_pred = model2.predict(x_test, batch_size=64, verbose=1)
+y_pred2 = np.argmax(y_pred, axis=1)
+
+
+print(classification_report(y_test, y_pred2))
+
+print('------------confusion_matrix-------------------')
+print(confusion_matrix(y_test, y_pred2, labels=[0, 1, 2]))
+
+
+
+
+plt.figure(figsize = (15,10))
+plt.plot(history2.history['accuracy'])
+plt.plot(history2.history['val_accuracy'])
+plt.legend(['Train2', 'Test2','Train1', 'Test1'], loc='upper left')
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
 
 
